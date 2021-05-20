46e6f7a92c5eba60ef6df5f09e8982f26976b2a1
lingchen <lingchen@migu.cn>
2021-05-20 16:52:22 +0800

    fix: 添加服务端渲染的功能

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,14 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Search from '../components/search'
 import LinkList from '../components/linkList'
+import { readFile } from 'fs'
+import { promisify } from 'util'
+import { join } from 'path'
 
-export default function Home() {
+const read = promisify(readFile)
+
+export default function Home({ data }) {
+  console.log(data)
   return (
     <div>
       <Head>
@@ -19,4 +25,30 @@ export default function Home() {
       
     </div>
   )
+}
+
+export async function getStaticProps () {
+  const data = await read(join(process.cwd(), 'pages', '_app.js'), 'utf-8')
+  // function fetchFavicon(url) {
+  //   return new Promise(async(resolve, reject) => {
+  //     const imageUrl = `chrome://favicon2/?size=24&scale_factor=1x&show_fallback_monogram=&page_url=${url}`
+  //     const data = await fetch(imageUrl)
+  //     const canvas = document.createElement("canvas");
+  //     canvas.width = 24
+  //     canvas.height = 24
+  //     const ctx = canvas.getContext("2d");
+  //     ctx.drawImage(canvas, 0, 0);
+  //     const dataURL = canvas.toDataURL("image/png");
+  //     resolve(dataURL)
+  //   });
+  // }
+
+  // const url = await fetchFavicon('https://www.baidu.com')
+  // console.log(url)
+
+  return {
+    props: {
+      data: url
+    }
+  }
 }
